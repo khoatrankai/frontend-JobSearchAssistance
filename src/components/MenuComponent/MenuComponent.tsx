7c201258f20e4bdac5a728cf4c97116d7efabe92
KhoaTT <khoanono963@gmail.com>
2023-12-25 18:59:31 +0700

    first commit3

--- src/components/MenuComponent/MenuComponent.tsx
+++ src/components/MenuComponent/MenuComponent.tsx
@@ -320,23 +320,23 @@ const MenuComponent = (props: Props) => {
       setOpenModalNoteCreateCompany(true);
       // return;
     }
-    if (profile && profile.roleData === 3) {
-      logEvent(analytics, "select_post_recruitment");
-      setTabMenu(false);
-      router.push("/banner-recruiter");
-    } else {
-      toast.warning("Vui lòng đăng nhập bằng tài khoản tuyển dụng", {
-        position: "bottom-center",
-        autoClose: 2000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-      });
-      setOpenModalLogin(true);
-    }
+    // if (profile && profile.roleData === 3) {
+    //   logEvent(analytics, "select_post_recruitment");
+    //   setTabMenu(false);
+    //   router.push("/banner-recruiter");
+    // } else {
+    //   toast.warning("Vui lòng đăng nhập bằng tài khoản tuyển dụng", {
+    //     position: "bottom-center",
+    //     autoClose: 2000,
+    //     hideProgressBar: false,
+    //     closeOnClick: true,
+    //     pauseOnHover: true,
+    //     draggable: true,
+    //     progress: undefined,
+    //     theme: "colored",
+    //   });
+    //   setOpenModalLogin(true);
+    // }
   };
 
   const IOSSwitch = styled((props: SwitchProps) => (
